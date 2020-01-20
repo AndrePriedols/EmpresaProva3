3943c17f39562d247c1ded94502f043469f1986f
andrepsouza <andrepsouza@localhost>
2020-01-20 09:18:21 -0200

    Regex Nome e Razao Social

--- src/main/java/br/com/contmatic/empresa/Utilitarios.java
+++ src/main/java/br/com/contmatic/empresa/Utilitarios.java
@@ -4,11 +4,23 @@ public class Utilitarios {
 
     private static final String REGEX_VALIDACAO_EMAIL = "^[a-zA-Z0-9_-][a-zA-Z0-9._-]+@[a-zA-Z0_9][a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$";
 
+    private static final String REGEX_NOME_VALIDO = "/ˆ[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÊÍÏÓÔÕÖÚÇÑ'\\s\\-]+$/";
+    
+    private static final String REGEX_RAZAO_SOCIAL_VALIDA = "";
+    
     public Utilitarios() {
     }
 
     public String getRegexValidacaoEmail() {
         return REGEX_VALIDACAO_EMAIL;
+    }
+    
+    public String getRegexNome() {
+    	return REGEX_NOME_VALIDO;
+    }
+    
+    public String getRegexRazaoSocial() {
+    	return REGEX_RAZAO_SOCIAL_VALIDA;
     }
     
     
